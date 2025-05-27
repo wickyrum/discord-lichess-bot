d53c82102d6292c812b8cdf89c3310cd55456c03
Vikram S <2301711027033@mcc.edu.in>
2025-05-28 00:29:46 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const {Client, IntentsBitField} = require('discord.js');   
+require('dotenv').config();
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.MessageContent,   
+    ]
+})
+
+async function challengeHandler(interaction) {  
+    if (!interaction.isCommand()) return;
+
+
+    if (interaction.commandName === 'create-challenge') {
+        const subcommand = interaction.options.getSubcommand();
+        const clock = interaction.options.getInteger('clock');
+        let increment = interaction.options.getInteger('increment');
+        if (increment === null) {
+            increment = 0; // Default to 0 if not provided
+        }
+        const lichessObj = await lichessCall(subcommand, clock, increment)
+        await interaction.reply(`${subcommand} match has been created, here is the link ${lichessObj}`)
+
+    }
+}
+
+async function lichessCall(sub_command, time, extra_time) {
+    const url = `https://lichess.org/api/challenge/open`
+    const header = {
+        'Authorization': `Bearer ${process.env.LICHESS_TOKEN}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    const data = new URLSearchParams({
+    'name': `${sub_command} challenge`,
+    'clock.limit': time,
+    'clock.increment': extra_time,
+    });
+
+    const alphaResponse = await fetch(url, {
+        method: 'POST',
+        headers: header,
+        body: data,
+    })
+    const betaResponse = await alphaResponse.json()
+    console.log(betaResponse.url)
+    return betaResponse.url
+    
+
+
+}
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+}); 
+
+client.on('interactionCreate', (interaction) => {
+    if (interaction.commandName === 'mame') {
+        interaction.reply('sollu mame');
+    }
+    if (interaction.commandName === 'vanakkam') {
+        interaction.reply('vanakkam sollu mame');
+    }   
+})
+
+client.on('interactionCreate', challengeHandler)
+
+client.login(process.env.DISCORD_TOKEN)
---
+++ src/register-commands.js
@@ -0,0 +1,157 @@
+require('dotenv').config();
+const {REST, Routes, ApplicationCommandOptionType, AutocompleteInteraction} = require('discord.js');   
+const commands = [
+    {
+        name: 'create-challenge',
+        description: 'creates a new lichess-challenge',
+        options: [
+            {
+                name: 'rapid',
+                description: 'creates a rapid challenge',
+                type: ApplicationCommandOptionType.Subcommand,
+                options: [
+                    {
+                        name: 'clock',
+                        description: 'sets the clock for the rapid challenge',
+                        type: 4,
+                        required: true,
+                        choices: [
+                            {
+                                name: '10 minutes',
+                                value: 600,
+                            },
+                            {
+                                name: '15 minutes',
+                                value: 900,
+                            },
+                            {
+                                name: '20 minutes',
+                                value: 1200,
+                            }
+
+                        ]
+                    },
+                    {
+                        name: 'increment',
+                        description: 'sets the increment for the rapid challenge',
+                        type: 4,
+                        choices: [
+                            {
+                                name: '5 seconds',
+                                value: 5,
+                            },
+                            {
+                                name: '10 seconds',
+                                value: 10,
+                            },
+                            {
+                                name: '15 seconds',
+                                value: 15,
+                            }
+
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'blitz',
+                description: 'creates a blitz challenge',
+                type: ApplicationCommandOptionType.Subcommand,
+                options: [
+                    {
+                        name: 'clock',
+                        description: 'sets the clock for the blitz challenge',
+                        type: 4,
+                        required: true,
+                        choices: [
+                            {
+                                name: '3 minutes',
+                                value: 180,
+                            },
+                            {
+                                name: '5 minutes',
+                                value: 300,
+                            },
+                        ]
+                    },
+                    {
+                        name: 'increment',
+                        description: 'sets the increment for the blitz challenge',
+                        type: 4,
+                        choices: [
+                            {
+                                name: '2 seconds',
+                                value: 2,
+                            },
+                            {
+                                name: '3 seconds',
+                                value: 3,
+                            },
+                            {
+                                name: '5 seconds',
+                                value: 5,
+                            }
+
+                        ]
+                    }
+                ]
+            },
+           {
+                name: 'bullet',
+                description: 'creates a bullet challenge',
+                type: ApplicationCommandOptionType.Subcommand,
+                options: [
+                    {
+                        name: 'clock',
+                        description: 'sets the clock for the bullet challenge',
+                        type: 4,
+                        required: true,
+                        choices: [
+                            {
+                                name: '1 minutes',
+                                value: 60
+                            },
+                            {
+                                name: '2 minutes',
+                                value: 120,
+                            },
+                        ]
+                    },
+                    {
+                        name: 'increment',
+                        description: 'sets the increment for the bullet challenge',
+                        type: 4,
+                        choices: [
+                            {
+                                name: '1 seconds',
+                                value: 1,
+                            },
+                            {
+                                name: '2 seconds',
+                                value: 2,
+                            },
+                        ]
+                    }
+                ]
+
+            }, 
+        ],
+        
+    
+    },
+];
+
+const rest = new REST({version: '10'}).setToken(process.env.DISCORD_TOKEN); 
+
+(async () => {
+    try {
+        console.log('starting putting commands')
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.BOT_ID, process.env.SERVER_ID),
+            {body: commands},
+        )
+        console.log('Successfully registered application commands.');
+    } catch (error) {
+        console.log(error);
+    }
+})()
