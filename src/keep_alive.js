eb5badf63c55cff392b5716bdd82c9d170a5a861
Vikram S <2301711027033@mcc.edu.in>
2025-05-30 22:05:13 +0530

    update

--- src/keep_alive.js
+++ src/keep_alive.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const { exec } = require('child_process')  
 const app = express()
 const port = process.env.PORT || 4000 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+// New route to run your script
+app.get('/run-tournament', (req, res) => {
+  exec('node create-tournament.js', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing script: ${error.message}`)
+      return res.status(500).send('Failed to run tournament script')
+    }
+    if (stderr) {
+      console.error(`Script stderr: ${stderr}`)
+    }
+    console.log(`Script output: ${stdout}`)
+    res.send('Tournament script executed!')
+  })
 })
 
 app.listen(port, () => {
