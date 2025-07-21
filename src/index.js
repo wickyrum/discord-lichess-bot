c6cee3a248bd83ad4b09ea23e151f74afc2dbf64
wickrum <wickristine@proton.me>
2025-07-22 00:02:19 +0530

    cron time change #4

--- src/index.js
+++ src/index.js
@@ -94,4 +94,4 @@ client.on('ready', () => {
 client.on('interactionCreate', challengeHandler)
 
 client.login(process.env.DISCORD_TOKEN)
-cron.schedule("50 23 * * 1", messagePut);
+cron.schedule("04 00 * * 2", messagePut);
